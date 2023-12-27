d33cdf8adb26cf4a6e914f6ff889c82cae77a446
rscode7 <154357069+rscode7@users.noreply.github.com>
2023-12-28 02:10:48 +0530

    hello

--- lib/Journey.dart
+++ lib/Journey.dart
@@ -1,18 +1,3 @@
 import 'package:flutter/material.dart';
-class JourneyPage extends StatelessWidget {
-  const JourneyPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar:AppBar(
-        title: Text('Plan Your Journey'),
-      ) ,
-      body: Column(
-        children: [
 
-        ],
-      ),
-    ));
-  }
-}
--- lib/home page.dart
+++ lib/home page.dart
@@ -90,5 +90,165 @@ class TestApp extends StatelessWidget {
 }
 
 
+class JourneyPage extends StatefulWidget {
+  const JourneyPage({Key? key});
+
+  @override
+  _JourneyPageState createState() => _JourneyPageState();
+}
+
+class _JourneyPageState extends State<JourneyPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _selectedDeparture;
+  String? _selectedDestination;
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedTime = TimeOfDay.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Plan Your Journey'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: 'Depart From',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: _selectedDeparture,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedDeparture = newValue;
+                    });
+                  },
+                  items: [
+                    'Select Station', // Default option
+                    'Station 1',
+                    'Station 2',
+                    'Station 3',
+                    // Add more stations as needed
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 16),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: 'Destination',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: _selectedDestination,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedDestination = newValue;
+                    });
+                  },
+                  items: [
+                    'Select Station', // Default option
+                    'Station A',
+                    'Station B',
+                    'Station C',
+                    // Add more stations as needed
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 16),
+                // Date Input Field
+                GestureDetector(
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: _selectedDate,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now().add(Duration(days: 365)),
+                    );
+                    if (pickedDate != null && pickedDate != _selectedDate) {
+                      setState(() {
+                        _selectedDate = pickedDate;
+                      });
+                    }
+                  },
+                  child: InputDecorator(
+                    decoration: InputDecoration(
+                      labelText: 'Select Date',
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.date_range),
+                    ),
+                    child: Text(
+                      '${_selectedDate.toLocal()}'.split(' ')[0],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                // Time Input Field
+                GestureDetector(
+                  onTap: () async {
+                    TimeOfDay? pickedTime = await showTimePicker(
+                      context: context,
+                      initialTime: _selectedTime,
+                    );
+                    if (pickedTime != null && pickedTime != _selectedTime) {
+                      setState(() {
+                        _selectedTime = pickedTime;
+                      });
+                    }
+                  },
+                  child: InputDecorator(
+                    decoration: InputDecoration(
+                      labelText: 'Select Time',
+                      border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.access_time),
+                    ),
+                    child: Text(
+                      '${_selectedTime.format(context)}',
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                // Show Route Button
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle show route logic
+                  },
+                  child: Text('Show Route'),
+                ),
+                SizedBox(height: 16),
+                // Reset Button
+                ElevatedButton(
+                  onPressed: () {
+                    _formKey.currentState?.reset();
+                    setState(() {
+                      _selectedDeparture = null;
+                      _selectedDestination = null;
+                      _selectedDate = DateTime.now();
+                      _selectedTime = TimeOfDay.now();
+                    });
+                  },
+                  child: Text('Reset'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 
