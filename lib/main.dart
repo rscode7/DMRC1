a64d9176547234372471301b4668d7e05cc89e63
rscode7 <154357069+rscode7@users.noreply.github.com>
2023-12-30 16:16:45 +0530

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,64 @@
+import 'dart:async';
+
 import 'package:dmrc1/home%20page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(home: TestApp()));
+  runApp( MaterialApp(home: SplashScreen()));
 }
 
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay before navigating to the home page
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TestApp()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome To DMRC!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the DMRC!'),
+      ),
+    );
+  }
+}
